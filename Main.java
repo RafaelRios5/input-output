a3b18820837fde1432dd86b841e774674d301e8c
RafaelRios5 <rafael.rios@hgs.hiddengeniusproject.org>
2021-11-13 19:55:38 +0000

    added inputand output

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+    System.out.println("what Is Your Favorite Sport?");
+  
+   
+    //type identifier = new type();
+    Scanner scanner = new Scanner(System.in);
+    String name = scanner.nextLine();
+
+    
+    
+    System.out.println("hello " + name);
+
+  }
+}
